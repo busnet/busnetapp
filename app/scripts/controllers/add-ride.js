cb2a3a451590191a42cb5b1150d12233f6cbfa71
Yehuda Tuvia <yehuda@busnet.co.il>
2015-05-03 18:43:36 +0300

    add new ride page

--- app/scripts/controllers/add-ride.js
+++ app/scripts/controllers/add-ride.js
@@ -18,14 +18,27 @@ angular.module('busnetApp.addride', ['busnetApp.grandfather'])
 	      resolve:{
 	      	ridetypes: function($http){
 	      		return $http.post('http://localhost:3002/rest/ridetypes').then(function(res){
+	      			console.log(res);
 	      			return _.map(res.data, function(item){
 	      				return {id: item._id, label: item.name};
 	      			});
 	      		});
-	      	}
+	      	},
+	      	vehicles: function($http){
+	      		return $http.post('http://localhost:3002/rest/vehicles').then(function(res){
+	      			return _.map(res.data, function(item){
+	      				return {id: item.name, label: item.name};
+	      			});
+	      		});
+	      	},
 	      }
-	    });
+	    });	
 	})
-  .controller('AddRideCtrl', function ($scope, ridetypes) {
+  .controller('AddRideCtrl', function ($scope, ridetypes, vehicles) {
     $scope.ridetypes = ridetypes;
+    $scope.vehicles = vehicles;
+    $scope.vehicleCount = ['1', '2', '3', '4','5+'];
+  	$('.date').datetimepicker({
+  		format: "DD/MM/YY"
+  	});
   });
